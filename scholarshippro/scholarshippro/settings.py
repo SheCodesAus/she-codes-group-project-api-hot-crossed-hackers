065077bf8f92e2cb5d7273426904dea49d6a7d0c
Sarah <sarahdavis398@gmail.com>
2022-08-27 13:11:34 +1000

    Update settings.py

--- scholarshippro/scholarshippro/settings.py
+++ scholarshippro/scholarshippro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','wphf&)k)(^3c@ygxf*p(!w=8yx&=epw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
