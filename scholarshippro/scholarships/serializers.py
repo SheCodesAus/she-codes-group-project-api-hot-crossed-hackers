a6270c01f6f40b477c56ce4584c15d544833dc89
Sarah <sarahdavis398@gmail.com>
2022-09-01 20:32:51 +1000

    creation of permissions, and scholarship detail serializer.

--- scholarshippro/scholarships/serializers.py
+++ scholarshippro/scholarships/serializers.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from rest_framework import serializers
 from .models import Scholarships 
 
@@ -14,3 +13,17 @@ class ScholarshipSerializer(serializers.Serializer):
 
     def create (self, validated_data):
         return Scholarships.objects.create(**validated_data)
+
+
+class ScholarshipDetailSerializer(ScholarshipSerializer):
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.organisation = validated_data.get('organisation', instance.organisation)
+        instance.description = validated_data.get('description', instance.description)
+        instance.image = validated_data.get('image', instance.image)
+        instance.url = validated_data.get('url', instance.url)
+        instance.closing_date = validated_data.get('closing_date', instance.closing_date)
+        instance.owner = validated_data.get('owner', instance.owner)
+        instance.save()
+        return instance
