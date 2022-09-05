89820f6059cbb26183f78ded8013485d7e60dc21
Sarah <sarahdavis398@gmail.com>
2022-09-05 18:46:13 +1000

    users create function in user serializer

--- scholarshippro/users/serializers.py
+++ scholarshippro/users/serializers.py
@@ -1,10 +1,19 @@
 
 from rest_framework import serializers
 from .models import CustomUser
+from django.http import Http404, HttpResponseBadRequest
+from django.contrib.auth.hashers import make_password
 class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        try:
+            validated_data['password'] = make_password(validated_data['password'])
+            return CustomUser.objects.create(**validated_data)
+        except KeyError:
+            raise HttpResponseBadRequest
 
     def create(self, validate_data):
         return CustomUser.objects.create(**validate_data)
