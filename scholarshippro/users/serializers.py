8554654afcee0e4ca179f688dd9741209bd12919
nivacm <nivacm@gmail.com>
2022-09-05 16:49:28 +1000

    delete-alter-user-information

--- scholarshippro/users/serializers.py
+++ scholarshippro/users/serializers.py
@@ -1,10 +1,26 @@
-
 from rest_framework import serializers
+from django.http import Http404, HttpResponseBadRequest
 from .models import CustomUser
+from django.contrib.auth.hashers import make_password
 class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
+    password = serializers.CharField(max_length=200)
 
     def create(self, validate_data):
-        return CustomUser.objects.create(**validate_data)
+        try: 
+            validate_data['password'] = make_password(validate_data['password'])
+            return CustomUser.objects.create(**validate_data)
+        except KeyError:
+            raise HttpResponseBadRequest
+
+    def update(self, instance, validate_data):
+        instance.username = validate_data.get('username', instance.username)
+        instance.email = validate_data.get('email', instance.email)
+        if "password" in validate_data.keys():
+            instance.password = make_password(validate_data.get('password'))
+        instance.save()
+        return instance
+
+        
